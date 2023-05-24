29cfa48a788794f1a539103dd03cbe4a7897f1c0
Abraham Musa <amusa26@gmail.com>
2023-05-23 22:07:03 -0400

    se agrega services, reposity y entity

---
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -0,0 +1,53 @@
+package org.aguzman.springcloud.msvc.usuarios.models.entity;
+
+
+import javax.persistence.*;
+import javax.websocket.ClientEndpoint;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String nombre;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.aguzman.springcloud.msvc.usuarios.repositories;
+
+import org.aguzman.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+}
